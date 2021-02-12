9e756e5443aaf188afee011a7334afed17fb1b9c
Mel <mkimble88@gmail.com>
2021-02-12 16:27:52 -0500

    import views to urls and add to url

--- urls.py
+++ urls.py
@@ -1,6 +1,8 @@
 import django
 from django.conf import settings
 from django.conf.urls import url
+from views import view_fastqc_html_report, get_version_json, trash_experiment,\
+    _delete_all_trashed, stats_ingestion_timeline
 
 # urlpatterns = patterns('')
 
@@ -12,19 +14,19 @@ seqfac_urls = [
     # url(r'^results/(?P<dataset_id>\d+)/(?P<filename>.*)$',
     #     'sequencing_facility.views.view_html_result_file'),
 
-    url(r'^report/(?P<dataset_id>\d+)/(?P<filename>.*)$',
-        'tardis.apps.sequencing_facility.views.view_fastqc_html_report'),
+    url(r'^report/(?P<dataset_id>\d+)/(?P<filename>.*)$',view_fastqc_html_report,
+        name='tardis.apps.sequencing_facility.views.view_fastqc_html_report'),
 
     # custom (non-tastypie) API endpoints
-    url(r'^api/version$',
-        'tardis.apps.sequencing_facility.custom_api.get_version_json'),
+    url(r'^api/version$',get_version_json,
+        name='tardis.apps.sequencing_facility.custom_api.get_version_json'),
 
-    url(r'^api/trash_experiment/(?P<experiment_id>\d+)$',
-        'tardis.apps.sequencing_facility.custom_api.trash_experiment'),
+    url(r'^api/trash_experiment/(?P<experiment_id>\d+)$',trash_experiment,
+        name='tardis.apps.sequencing_facility.custom_api.trash_experiment'),
 
-    url(r'^api/_delete_all_trashed/$',
-        'tardis.apps.sequencing_facility.custom_api._delete_all_trashed'),
+    url(r'^api/_delete_all_trashed/$',_delete_all_trashed,
+        name='tardis.apps.sequencing_facility.custom_api._delete_all_trashed'),
 
-    url(r'^api/_stats/$',
-        'tardis.apps.sequencing_facility.custom_api.stats_ingestion_timeline'),
+    url(r'^api/_stats/$',stats_ingestion_timeline,
+        name='tardis.apps.sequencing_facility.custom_api.stats_ingestion_timeline'),
 ]
