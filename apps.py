5f6048d1b4b0b6a08768f53d95d35c0b8c2dc910
Mel <mkimble88@gmail.com>
2021-02-12 17:20:16 -0500

    remove app deps in apps.py

--- apps.py
+++ apps.py
@@ -4,4 +4,4 @@ from tardis.app_config import AbstractTardisAppConfig
 class SequencingFacilityConfig(AbstractTardisAppConfig):
     name = 'tardis.apps.sequencing_facility'
     verbose_name = 'Sequencing Facility'
-    app_dependencies = ['storages']
+#    app_dependencies = ['storages']
